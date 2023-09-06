0366e690bbf6a9627334e5b56f043b5c733d7c35
nii_okantey <nunoogideon19@gmail.com>
2023-09-06 18:27:48 +0000

    updates

--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -1,11 +1,14 @@
 import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default Skip = () => {
+  const navigation = useNavigation();
   return (
     <View className="w-full flex flex-row justify-end items-center">
       <Text
         className="text-lg text-purple-950"
         style={{ fontFamily: "inter-bold" }}
+        onPress={() => navigation.navigate("Welcome")}
       >
         SKIP
       </Text>
